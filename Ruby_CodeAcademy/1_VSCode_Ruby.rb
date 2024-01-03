7bcca58dadb482ff8907c3dbf440c984f11e0e7b
Kaio Monte Cristo <113322785+Kelbertt@users.noreply.github.com>
2024-01-03 16:52:33 -0300

    One of first projects in ruby

---
+++ Ruby_CodeAcademy/1_VSCode_Ruby.rb
@@ -0,0 +1,20 @@
+
+print "What is your first name?"
+
+first_name = gets.chomp
+first_name.capitalize!
+
+print "What is your last name?"
+last_name = gets.chomp
+last_name.capitalize!
+
+print "What city are you from?"
+city = gets.chomp
+city.capitalize!
+
+print "What state or province are you from?"
+
+state = gets.chomp
+state.capitalize!
+
+puts "Your name is #{first_name} #{last_name} and you are from #{city}, #{state}!"
