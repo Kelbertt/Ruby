1848355d0ab162cad3fa9b3a79ad2775cada2577
Kaio Monte Cristo <113322785+Kelbertt@users.noreply.github.com>
2024-02-18 23:16:39 -0300

    create questionary.rb

---
+++ Ruby_CodeAcademy/Pessoal_Informations/Questions.rb
@@ -0,0 +1,19 @@
+print "What is your first name?"
+first_name = gets.chomp
+first_name2 = first_name.capitalize
+first_name.capitalize!
+
+print "What's your last name?"
+last_name = gets.chomp
+last_name2 = last_name.capitalize
+last_name.capitalize!
+
+print "what city are you from?"
+city = gets.chomp
+city2 = city.capitalize
+city.capitalize!
+
+print "What state or province are you from "
+state = gets.chomp
+state2 = state.capitalize
+puts state.upcase! 
