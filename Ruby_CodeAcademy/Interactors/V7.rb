33617b61a9a96ceb12ddbf7187482b050427a2b8
Kaio Monte Cristo <113322785+Kelbertt@users.noreply.github.com>
2024-02-18 23:29:18 -0300

    Create V7.rb

---
+++ Ruby_CodeAcademy/Interactors/V7.rb
@@ -0,0 +1,10 @@
+lunch_order = {
+  "Ryan" => "wonton soup",
+  "Eric" => "hamburger",
+  "Jimmy" => "sandwich",
+  "Sasha" => "salad",
+  "Cole" => "taco"
+}
+
+lunch_order.each {|key, value| puts "#{value}"}
+
