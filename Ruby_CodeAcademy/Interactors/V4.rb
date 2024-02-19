5025eb320407976aac1a630916c42d4da0cf2e41
Kaio Monte Cristo <113322785+Kelbertt@users.noreply.github.com>
2024-02-18 23:26:21 -0300

    Create V4.rb

---
+++ Ruby_CodeAcademy/Interactors/V4.rb
@@ -0,0 +1,16 @@
+secret_identities = {
+  "The Batman" => "Bruce Wayne",
+  "Superman" => "Clark Kent",
+  "Wonder Woman" => "Diana Prince",
+  "Freakazoid" => "Dexter Douglas",
+  "The Flash"  => "Barry Allen",
+  "Green Arrow" => "Oliver Queen",
+  "Green Lantern" => "Hal Jordan",
+  "Cyborg"      => "Victor Stone",
+  "Nightwing"   => "Dick Grayson",
+  "Robin"       => "Damian Wayne",
+
+
+}
+  
+secret_identities.each {|hero, secretidentities| puts "#{hero}: #{secretidentities}"}
